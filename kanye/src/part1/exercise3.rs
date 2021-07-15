e81f886f06c6ec90e95e5b54fa8934c2977bc863
Elroy Kanye <elroykanye@gmail.com>
2021-07-15 02:53:01 +0100

    added first variant with hardcoded 7

---
+++ kanye/src/part1/exercise3.rs
@@ -0,0 +1,8 @@
+// Function to print the multiplication table of 7
+pub fn mult_table_seven() {
+    println!("\nMultiplication table of 7\n");
+    for i in 1..20 {
+        println!("7 * {} = {}", i, 7*i);
+    }
+    println!("\n\n");
+}
