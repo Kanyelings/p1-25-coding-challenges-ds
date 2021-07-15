a140e89bdbc35662bc81ce1498d55379279929d7
Elroy Kanye <elroykanye@gmail.com>
2021-07-15 02:06:32 +0100

    completed 2 variants of exercise 1

---
+++ kanye/src/part1/exercise1.rs
@@ -0,0 +1,17 @@
+// Function that will print numbers from 1 to 10
+
+pub fn print_one_to_ten() {
+    for j in 1..11 {
+        println!("j = {}", j);
+    }
+}
+
+/*
+pub fn print_one_to_ten2() {
+    let mut i = 1;
+    while i < 11 {
+        println!("i = {}", i);
+        i += 1;
+    }
+}
+*/
