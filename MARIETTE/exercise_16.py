1ced2c262009d06a3de7f004ce83031f1c0a984e
Nji-Mariette <marietteneh@gmail.com>
2021-07-18 09:26:38 -0700

    Done with <exercise_16.py>

---
+++ MARIETTE/exercise_16.py
@@ -0,0 +1,8 @@
+y=int(input("enter a number:"))
+def IsPrime(y):
+    if y >1:
+        for i in range(2,y):
+            if y % n==0:
+                print (false)
+        else:
+            print(prime)
