60c029ec0c12961302e6bbc00eaebe4b19f8b5bc
chelsea-banke <bankechelsea@gmail.com>
2021-07-26 23:44:03 +0100

    Create challenge_29.java
    challenge_29 is a java class having a method which takes in two numbers of undefined sizes as strings and returns their sums as a string.

---
+++ Chelsea/challenge_29.java
@@ -0,0 +1,7 @@
+package com.test;
+
+public class challenge_29 {
+    public static String indefiniteSum(String n1, String n2){
+        return Long.toString(Long.parseLong(n1) + Long.parseLong(n2));
+    }
+}
