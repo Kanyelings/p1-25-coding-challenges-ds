6d565372abd065cb5bcd2025c8f130bd9f876b79
chelsea-banke <bankechelsea@gmail.com>
2021-07-26 21:00:24 +0100

    updating challenge 22 and rewriting challenge 24
    challenge 22 is a class which contains methods capable of reversing an array of strings, integers, and boolean values.
    
    while challenge 24 is a class containing methods capable of merging two arrays of strings, integers, and boolean.

--- Chelsea/challenge_24.java
+++ Chelsea/challenge_24.java
@@ -1,31 +1,49 @@
 package com.test;
 
 public class challenge_24 {
-    public static int[] reverse(int[] arr){
-        int[] rev = new int[arr.length];
-        int ind = arr.length - 1;
-        for (int i = 0; i<arr.length; i++){
-            rev[i] = arr[ind];
-            ind -= 1;
+    public static int[] merge(int[] arr1, int[] arr2){
+        int[] ret = new int[arr1.length + arr2.length];
+        int temp = 0, temp0 = 0;
+        for (int i=0; i < arr1.length + arr2.length; i++){
+            if (temp < arr1.length) {
+                ret[i] = arr1[i];
+                temp += 1;
+            }
+            else{
+                ret[i] = arr2[temp0];
+                temp0 += 1;
+            }
         }
-        return rev;
+        return ret;
     }
-    public static String[] reverse(String[] arr){
-        String[] rev = new String[arr.length];
-        int ind = arr.length - 1;
-        for (int i = 0; i<arr.length; i++){
-            rev[i] = arr[ind];
-            ind -= 1;
+    public static String[] merge(String[] arr1, String[] arr2){
+        String[] ret = new String[arr1.length + arr2.length];
+        int temp = 0, temp0 = 0;
+        for (int i=0; i < arr1.length + arr2.length; i++){
+            if (temp < arr1.length) {
+                ret[i] = arr1[i];
+                temp += 1;
+            }
+            else{
+                ret[i] = arr2[temp0];
+                temp0 += 1;
+            }
         }
-        return rev;
+        return ret;
     }
-    public static boolean[] reverse(boolean[] arr){
-        boolean[] rev = new boolean[arr.length];
-        int ind = arr.length - 1;
-        for (int i = 0; i<arr.length; i++){
-            rev[i] = arr[ind];
-            ind -= 1;
+    public static boolean[] merge(boolean[] arr1, boolean[] arr2){
+        boolean[] ret = new boolean[arr1.length + arr2.length];
+        int temp = 0, temp0 = 0;
+        for (int i=0; i < arr1.length + arr2.length; i++){
+            if (temp < arr1.length) {
+                ret[i] = arr1[i];
+                temp += 1;
+            }
+            else{
+                ret[i] = arr2[temp0];
+                temp0 += 1;
+            }
         }
-        return rev;
+        return ret;
     }
 }
