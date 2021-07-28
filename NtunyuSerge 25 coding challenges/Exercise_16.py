4aba43f2d30a97114ca85d987385635b6c61ce30
Ntunyu Serge <81696072+Serge-NSN@users.noreply.github.com>
2021-07-28 12:46:52 +0100

    Update Exercise_16.py
    Changed function name from prime_or_not to is_prime
    Used a flag variable and deleted the unnecessary lines of code

--- NtunyuSerge 25 coding challenges/Exercise_16.py
+++ NtunyuSerge 25 coding challenges/Exercise_16.py
@@ -1,16 +1,14 @@
 #  Create a function that will return a Boolean specifying if a number is prime
 
-def prime_or_not(number):
+def is_prime(number):
     if number <= 1:
-        result = False
-    elif number == 2:
-        result = True
+        flag = False
+    flag = False
     for i in range(2, number):  # Why is there an error when number=5 with loop running from 2 to (number//2)?
-        if number % i == 0:
-            result = False
+        if number % i != 0:
+            flag = True
             break
-        result = True
-    return result
+    return flag
 
 
 print(prime_or_not(37))
