6c53e62e489727c90ab5ee990b0c00e58a79fb6b
Signeemmanuel <signeemmanuel28@gmail.com>
2023-02-09 14:19:23 +0100

    Add exercise_09.cpp

---
+++ Signe_Josue/exercise_09.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+
+/**
+ * fahrenheit_to_celsius - Converts temperature in Fahrenheit to Celsius.
+ * @fah: temperature in fahrenheit.
+ * 
+ * Return: temperature in celsius.
+ */
+float fahrenheit_to_celsius(float fah)
+{
+    return ((fah - 32) / 1.8);
+}
