b982807e0acfa0bf69e51128c77a125e7980a42e
FlyntDenzel <93846700+FlyntDenzel@users.noreply.github.com>
2023-02-10 09:00:02 +0100

    Add files via upload

---
+++ FlyntDenzel/Exercise_15/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace Exercise
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter the nth number of the Fibonacci Series : ");
+            int nth_num = int.Parse(Console.ReadLine());
+
+            nth_num = nth_num - 1;
+            Console.Write(nth_fibonacci(nth_num));
+            Console.ReadKey();
+        }
+
+        private static int nth_fibonacci(int number)
+        {
+            if ((number == 0) || (number == 1))
+            {
+                return number;
+            }
+            else
+            {
+                return (nth_fibonacci(number - 1) + nth_fibonacci(number - 2));
+            }
+        }
+    }
+}
