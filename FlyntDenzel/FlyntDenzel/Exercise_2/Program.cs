00b23bc2ddff3465965229fbd5643fa24c683648
FlyntDenzel <93846700+FlyntDenzel@users.noreply.github.com>
2023-02-13 00:10:34 +0100

    Add files via upload

---
+++ FlyntDenzel/FlyntDenzel/Exercise_2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Exercise
+{
+    class Program
+    {
+        static void Main(string[] args){
+            for (int i = 1; i < 100; i+=2)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
