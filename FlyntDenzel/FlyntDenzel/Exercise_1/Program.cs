9af53b64fb04144374eb92d637c7f9b2f86f96cf
FlyntDenzel <93846700+FlyntDenzel@users.noreply.github.com>
2023-02-13 09:12:24 +0100

    Details of the exercise (#14)
    * Update README.md
    
    * Update README.md
    
    * Create FlyntDenzel
    
    * Delete FlyntDenzel
    
    * Create README.txt
    
    * Add files via upload
    
    uploaded exercise 1 and 2
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    Deleted build files
    
    * Delete FlyntDenzel directory
    
    * Create README.txt
    
    * Add files via upload
    
    * Update README.txt

---
+++ FlyntDenzel/FlyntDenzel/Exercise_1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Exercise_1
+{
+    class Program
+    {
+        static void Main(string[] args){
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.WriteLine(i);
+                }
+        }
+    }
+}
